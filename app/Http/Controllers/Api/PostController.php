e2e09118a6e1fd150c73558a199e9ed85842fb61
zaenalrfn <ZAOfficial24@gmail.com>
2024-12-30 13:26:52 +0700

    insert data posts

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -9,6 +9,8 @@ use App\Models\Post;
 use App\Http\Resources\PostResource;
 
 use App\Http\Controllers\Controller;
+//import facade Validator
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -19,5 +21,35 @@ class PostController extends Controller
         $posts = Post::latest()->paginate(5);
 
         return new PostResource(true, 'Data berhasil diambil', $posts);
+    }
+
+    public function store(Request $request)
+    {
+        // validate data
+        $validator = Validator::make($request->all(), [
+            'image'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title'     => 'required',
+            'content'   => 'required',
+        ]);
+
+        // check validator
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()
+            ], 401);
+        }
+        // upload image
+        $image = $request->file('image');
+        $image->storeAs('public/images', $image->hashName());
+
+        // create data
+        $post = Post::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $request->content,
+        ]);
+
+        return new PostResource(true, 'Data berhasil ditambahkan', $post);
     }
 }
